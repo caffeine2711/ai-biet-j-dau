eea301b35ad827af90185af63b36bcf093a5ef5e
caffeine2711 <85494515+caffeine2711@users.noreply.github.com>
2024-08-12 22:59:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bạn à! -____-',
-    introDesc: `Cổ nhân có câu:
-    zảo shang hảo tung của
-    xiên zai wuo zhou Bing Chilling
-    wuo han xi khoan Bing Chilling`,
-    btnIntro: '😒😒😒',
-    title: 'Mua đầy đủ rồi xin địa chỉ để ship thì không cho -_-',
-    desc: 'Làm 3 thằng hôm qua uống bia ăn bim bim',
-    btnYes: 'Nhấn vào đây để cho địa chỉ',
-    btnNo: 'Để suy nghĩ lại đã',
-    question:'Cho rồi thì nhập địa chỉ vào đây nha ',
+    introTitle: 'háloo bạn nhỏ',
+    introDesc: `lại là anh đây....
+    thực sự thì nhiều lúc nhớ em hong biết làm gì cả
+    nên là ngồi nghịch nghịch cái này =))))
+    làm một tí trắc nghiệm nhéeeee`,
+    btnIntro: '😏😏😏',
+    title: 'Pudding oiiii!! Em có yêu anh không???',
+    desc: 'Nghĩ cho kỹ vào nhá!! Anh biết em sẽ chọn gì đấy😏',
+    btnYes: 'Ghét anh....',
+    btnNo: 'Yêu anh rất rất nhiều',
+    question:'Biết ngay mà (´。＿。｀), người ta có yêu mình đâu....cho anh biết lý do đi T_T',
     btnReply: 'Gửi :D',
-    reply: 'Để gửi địa chỉ nhà qua facebook cho ha :D',
-    mess: 'hehehe, được luôn 😎',
-    messDesc: 'Tối nay 7h có mặt trước cửa nhà nhá',
-    btnAccept: 'eheheheheh',
+    reply: 'Bởi vì em yêu anh nhiều quá nên phải ghét cho bớt yêu đi',
+    mess: 'Anh biết mòooooo 😎',
+    messDesc: 'Anh cũng yêu bạn nhỏ của anh nhiều lắm ><',
+    btnAccept: 'Tới đây nào babeeeeee',
     messLink: 'https://www.facebook.com/3107.lilraven'
 }
